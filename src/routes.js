99b3bb02142ae7dfb735b56c4d8adb63472b6365
unknown <danizasalsabila12@gmail.com>
2022-05-11 14:44:19 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+const {
+  // eslint-disable-next-line max-len
+  addBooksHandler, getAllBooksHandler, getBooksbyIdHandler, editBooksByIdHandler, deleteBooksByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBooksbyIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBooksByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBooksByIdHandler,
+  },
+];
+
+module.exports = routes;
